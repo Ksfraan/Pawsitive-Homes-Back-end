f2de068acc0fcdcd9e1726636fefd13fc7e67206
“Carina <ksfraan@hotmail.com>
2024-01-09 15:24:29 +0000

    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use((req, res, next) => {
